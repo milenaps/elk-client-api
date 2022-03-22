57c560ec1f2718670da01b5e32871bafdb5b8d94
milenaps <milenacroft@gmail.com>
2022-03-21 23:36:54 -0300

    Implemented the delete service

--- src/main/java/com/mps/elk/sample/api/PetController.java
+++ src/main/java/com/mps/elk/sample/api/PetController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/pets")
@@ -16,7 +17,7 @@ public class PetController {
     private PetService service;
 
     @GetMapping
-    public List<Pet> getPets(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age, @RequestParam(required = false) String species) throws IOException {
+    public List<Map> getPets(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age, @RequestParam(required = false) String species) throws IOException {
         return service.list(name, age, species);
     }
 
@@ -26,11 +27,7 @@ public class PetController {
     }
 
     @DeleteMapping
-    public boolean deletePet(@RequestParam String name, @RequestParam Integer age, @RequestParam String species) {
-        Pet pet = new Pet();
-        pet.setName(name);
-        pet.setAge(age);
-        pet.setSpecies(species);
-        return service.delete(pet);
+    public boolean deletePet(@RequestParam String name, @RequestParam Integer age, @RequestParam String species) throws IOException {
+        return service.delete(name, age, species);
     }
 }
--- src/main/java/com/mps/elk/sample/service/PetService.java
+++ src/main/java/com/mps/elk/sample/service/PetService.java
@@ -2,22 +2,29 @@ package com.mps.elk.sample.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mps.elk.sample.model.Pet;
+import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
 public class PetService {
+
+    private static final String ES_INDEX = "pets";
 
     private RestHighLevelClient client;
     private ObjectMapper mapper;
@@ -28,29 +35,60 @@ public class PetService {
         mapper = new ObjectMapper();
     }
 
-    public List<Pet> list(String name, Integer age, String species) throws IOException {
-        SearchRequest searchRequest = new SearchRequest("pets");
+    public List<Map> list(String name, Integer age, String species) throws IOException {
+        SearchHit[] hits = getSearchHits(name, age, species, true);
+        if (hits == null) return null;
 
-        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
-        SearchHit[] searchHits = response.getHits().getHits();
-
-        return Arrays.stream(searchHits)
-                .map(hit -> mapper.convertValue(hit.getSourceAsString(), Pet.class))
+        return Arrays.stream(hits)
+                .map(hit -> mapper.convertValue(hit.getSourceAsMap(), Map.class))
                 .collect(Collectors.toList());
     }
 
     public Pet save(Pet pet) throws IOException {
         String jsonObject = String.format("{\"age\":%s,\"name\":\"%s\","
                 + "\"species\":\"%s\"}", pet.getAge(), pet.getName(), pet.getSpecies());
-        IndexRequest request = new IndexRequest("pets");
+        IndexRequest request = new IndexRequest(ES_INDEX);
         request.source(jsonObject, XContentType.JSON);
 
         client.index(request, RequestOptions.DEFAULT);
         return pet;
     }
 
-    public boolean delete(Pet pet) {
-        //TODO Delete pet from ES index
-        return false;
+    public boolean delete(String name, Integer age, String species) throws IOException {
+        SearchHit[] hits = getSearchHits(name, age, species, false);
+        if (hits == null) return false;
+
+        for (SearchHit hit : hits) {
+            DeleteRequest request = new DeleteRequest(ES_INDEX, hit.getId());
+            client.delete(request, RequestOptions.DEFAULT);
+        }
+        return true;
+    }
+
+    private SearchHit[] getSearchHits(String name, Integer age, String species, boolean isPartialSearch) throws IOException {
+        SearchRequest searchRequest = new SearchRequest(ES_INDEX);
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        if (isPartialSearch) {
+            if (StringUtils.hasText(name)) {
+                builder.query(QueryBuilders.termQuery("name", name));
+            }
+            if (age != null) {
+                builder.query(QueryBuilders.termQuery("age", age));
+            }
+            if (StringUtils.hasText(species)) {
+                builder.query(QueryBuilders.termQuery("species", species));
+            }
+            searchRequest.source(builder);
+        } else {
+            builder.query(QueryBuilders.matchQuery("name", name));
+            builder.query(QueryBuilders.matchQuery("age", age));
+            builder.query(QueryBuilders.matchQuery("species", species));
+        }
+        searchRequest.source(builder);
+
+        SearchResponse response = client.search(searchRequest, RequestOptions.DEFAULT);
+        if (response == null || response.getHits() == null) return null;
+
+        return response.getHits().getHits();
     }
 }
